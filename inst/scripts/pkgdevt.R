c41d9a405014cdddfc32b1b6ad70a54c33d6449b
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-11-15 22:38:35 -0500

    setup: update pkgdevt script

--- inst/scripts/pkgdevt.R
+++ inst/scripts/pkgdevt.R
@@ -14,7 +14,11 @@ usethis::use_roxygen_md()
 usethis::use_package_doc()
 
 usethis::use_testthat()
+
+fs::file_create("R/data.R")
+
 usethis::use_data_raw("claims")
+usethis::use_data_raw("triangles")
 usethis::use_data_raw("exposures")
 usethis::use_data_raw("industry")
 
@@ -22,3 +26,8 @@ usethis::use_vignette("A-actuarial-loss-reserving-overview", "Actuarial Loss Res
 usethis::use_vignette("B-simulation-of-claims-data", "Simulation of Claims Data")
 usethis::use_vignette("C-triangles", "Loss Development Triangles")
 usethis::use_vignette("D-ldfs", "Loss Development Factors")
+
+usethis::use_r("utils")
+usethis::use_r("simulate_claims")
+usethis::use_r("loss_run")
+usethis::use_r("triangles")
